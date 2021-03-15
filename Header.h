0052ab00fa57cf2fccc141d850399e745eb382c4
Диана Васеева <diana.vaseeva@mail.ru>
2021-03-16 00:42:56 +0300

    aaa

--- Header.h
+++ Header.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef SOME_UNIQUE_NAME_HERE
+#define SOME_UNIQUE_NAME_HERE
 #include <string>
 void ltoao(long number, std::string& res);
-
+#endif SOME_UNIQUE_NAME_HERE
--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <cmath>
 #include <string>
 #include "Header.h"
-
 int main() {
 	long num;
 	std::cin >> num;
