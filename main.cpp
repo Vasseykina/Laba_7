248aa14e8fc46e49f36cc18fc26be1d7d2ce4f6f
Диана Васеева <diana.vaseeva@mail.ru>
2021-03-16 00:25:22 +0300

    done

--- main.cpp
+++ main.cpp
@@ -7,9 +7,7 @@
 int main() {
 	long num;
 	std::cin >> num;
-	std::string s[256];
-	ltoao(num, *s);
-	for (int i = 0; i < (sizeof(s) / sizeof(s[1])); ++i) {
-		std::cout << s[i];
-	}
+	std::string s;
+	ltoao(num, s);
+	std::cout << s;
 }
